aeeda76e832a2f0fc8b23283dc7e0b3218e51d03
Dias Sunnatilla <diassunnatilla@MacBook-Air-Dias.local>
2025-05-01 19:49:52 +0500

    fixes

--- index.js
+++ index.js
@@ -3,37 +3,44 @@
     const items = track.children;
     const clone = track.innerHTML;
 
-  track.innerHTML += clone;
-    const menuButton = document.querySelectorAll
-    ('.btn-wrap');
-    const sideMenu = document.getElementById('sideMenu');
-
-    menuButton.addEventListener('click', () => {
-    sideMenu.classList.toggle('active');
-    });
+    track.innerHTML += clone;
 
 
     document.addEventListener('DOMContentLoaded', () => {
-        const bars = document.querySelectorAll('.bar-fill');
-      
-        const observer = new IntersectionObserver(entries => {
-          entries.forEach(entry => {
-            if (entry.isIntersecting) {
-              const bar = entry.target;
-              const targetWidth = bar.getAttribute('data-width');
-              bar.style.setProperty('--target-width', targetWidth);
-              bar.classList.add('filled');
-              observer.unobserve(bar);
-            }
-          });
-        }, {
-          threshold: 0.5
-        });
-      
-        bars.forEach(bar => {
-          observer.observe(bar);
-        });
+      // Карусель
+      const track = document.getElementById('carousel-track');
+      const items = track.children;
+      const clone = track.innerHTML;
+      track.innerHTML += clone;
+    
+      // Бургер-меню
+      const menuButton = document.querySelector('.btn-wrap');
+      const sideMenu = document.getElementById('sideMenu');
+    
+      menuButton.addEventListener('click', () => {
+        sideMenu.classList.toggle('active');
       });
+    
+      // Прогресс-бары
+      const bars = document.querySelectorAll('.bar-fill');
+      const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            const bar = entry.target;
+            const targetWidth = bar.getAttribute('data-width');
+            bar.style.setProperty('--target-width', targetWidth);
+            bar.classList.add('filled');
+            observer.unobserve(bar);
+          }
+        });
+      }, {
+        threshold: 0.5
+      });
+    
+      bars.forEach(bar => {
+        observer.observe(bar);
+      });
+    });
       
 
     
